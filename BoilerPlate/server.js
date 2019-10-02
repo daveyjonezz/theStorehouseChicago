436271e7094033feb95eed6a8c34aa816123a354
Davis Birsan <davis.birsan@gmail.com>
2019-10-01 20:06:47 -0500

    heroku push v3

--- BoilerPlate/server.js
+++ BoilerPlate/server.js
@@ -17,7 +17,7 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theStoreHouse");
-
+  
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
