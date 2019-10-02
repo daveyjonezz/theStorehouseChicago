f38f531376fb19122cd822c8751154d42ea235a2
Davis Birsan <davis.birsan@gmail.com>
2019-10-01 20:42:50 -0500

    heroku v7

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 } 
-// Add routes, both API and view
+// Add routes, both API and view 
 app.use(routes); 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theStoreHouse");
