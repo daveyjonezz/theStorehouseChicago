cdd8b5ac21fb6e95d76aa687b6c4fc0c49264a34
Davis Birsan <davis.birsan@gmail.com>
2019-10-01 20:45:50 -0500

    push up

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes); 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theStoreHouse");
-
+  
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
