efe7c36972dd7acb3d959a77d8a5041889736b55
Davis Birsan <davis.birsan@gmail.com>
2019-10-01 20:33:49 -0500

    heroku v6

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+} 
 // Add routes, both API and view
 app.use(routes); 
 // Connect to the Mongo DB
