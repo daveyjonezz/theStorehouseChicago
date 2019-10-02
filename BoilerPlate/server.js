3ef06d727a62857384dc87c2e96e790e46e24a77
Davis Birsan <davis.birsan@gmail.com>
2019-10-01 20:14:13 -0500

    heroku push v4

--- BoilerPlate/server.js
+++ BoilerPlate/server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 }
 // Add routes, both API and view
 app.use(routes);
-
+ 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/theStoreHouse");
 
