74ba5ef6d58f5a00f0c3a0a06e6795b3340d218a
Davis Birsan <davis.birsan@gmail.com>
2019-10-03 21:48:50 -0500

    eboard

--- client/src/components/Carousel/index.js
+++ client/src/components/Carousel/index.js
@@ -7,19 +7,19 @@ function Carousel({children}) {
 
 <div id="slideshow2" className="text-center">
    <div>
-     <img className="eboardimg" src="/images/adi.jpg" alt="eboard1"/>
+     <img className="eboardimg" src="/images/adi-eboard.jpg" alt="eboard1"/>
      <h2 style={{padding:"10px"}}>Adi Cepela</h2>
    </div>
    <div>
-     <img className="eboardimg"src="/images/davis.jpg" alt="eboard2"/>
+     <img className="eboardimg"src="/images/davis-eboard.jpg" alt="eboard2"/>
      <h2 style={{padding:"10px"}}>Davis Birsan</h2>
    </div>
    <div>
-     <img className="eboardimg"src="/images/jenni.jpg" alt="eboard3"/>
+     <img className="eboardimg"src="/images/jenni-eboard.jpg" alt="eboard3"/>
      <h2 style={{padding:"10px"}}>Jenni Birsan</h2>
    </div>
    <div>
-     <img className="eboardimg"src="/images/jimi.jpg" alt="eboard4"/>
+     <img className="eboardimg"src="/images/jimi-eboard.jpg" alt="eboard4"/>
      <h2 style={{padding:"10px"}}>Jimi Loc Nguyen</h2>
    </div>
 
